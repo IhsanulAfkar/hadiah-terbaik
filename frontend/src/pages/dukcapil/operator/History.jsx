5b536f0128fc0ee66935c8fdd57549ab8fe43af3
nasruladitri <nasruladitri2@gmail.com>
2026-01-08 13:18:42 +0700

    update

--- frontend/src/pages/dukcapil/operator/History.jsx
+++ frontend/src/pages/dukcapil/operator/History.jsx
@@ -10,7 +10,7 @@ import Pagination from '../../../components/ui/Pagination';
 import { Card, CardContent } from '../../../components/ui/Card';
 import { Search } from 'lucide-react';
 import { toast } from 'react-toastify';
-import { useAuth } from '../../../contexts/AuthContext';
+import { useAuth } from '../../../context/AuthContext';
 
 const History = () => {
     const navigate = useNavigate();
